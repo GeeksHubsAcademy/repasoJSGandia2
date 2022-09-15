27277529daac4cb3ccfebfd540bd925fe2c54b7e
Dave86dev <davidochando1986@gmail.com>
2022-09-15 12:54:22 +0200

    Bucles implementados en práctica

--- js/main.js
+++ js/main.js
@@ -99,3 +99,131 @@
 // }else{
 //     console.log("Comemos patata");
 // }
+
+// let edad = 20;
+
+// let estado = "Florida";
+
+// if(edad >= 21){
+
+//     if(estado == "Florida"){
+
+//         console.log("Enhorabuena, puedes probar el horrible sabor de Cruzcampo");
+//     };
+// };
+
+// if((edad >= 21) && (estado == "Florida")){
+//     console.log("Bebe cerveza con los caimanes");
+// }
+
+// let dia = "viernes";
+
+// if(dia == "sabado" || dia == "domingo"){
+//     console.log("podreis dormir un poquito más");
+// }
+
+// let tiempoLibre = false;
+
+// if(!tiempoLibre){
+//     console.log("urbalaaaaaaaaaaaaaaaaaaab nooooooooooooo");
+// }
+
+// let coche = "Mazda";
+
+// if(coche != "Mercedes"){
+//     console.log("Enhorabuena, eres original");
+// }
+
+
+// let nota = parseInt(prompt("Dime que nota has sacado"));
+
+// if(nota >= 0 && nota <= 4){
+//     console.log("insuficiente");
+// } else if (nota >= 5 && nota <= 6){
+//     console.log("suficiente");
+// } else if (nota >= 7 && nota <= 8){
+//     console.log("notable");
+// } else if (nota >= 9 && nota <= 10){
+//     console.log("excelente");
+// };
+
+// switch (nota) {
+
+//     case 0:
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//         console.log("Insuficiente");
+//     break;
+
+//     case 5:
+//     case 6:
+//         console.log("Suficiente");
+//     break;
+
+//     case 7:
+//     case 8:
+//         console.log("Notable");
+//     break;
+
+//     case 9:
+//     case 10:
+//         console.log("Excelente");
+//     break;
+
+//     default:
+//         console.log("No se que has puesto, pero estás suspendido");
+//     break;
+// }
+
+
+// let edad = 17;
+
+// let pelicula = "Jaws";
+
+// if(edad < 18){
+//     console.log("No puedes ver Jaws");
+// }else{
+//     console.log("Si puedes ver Jaws");
+// };
+
+//Mientras tanto, en el mundo de las ternarias.....
+
+// let edad = 17;
+
+// let verPelicula = edad < 18 ? "No puedes ver Jaws" : "Si puedes ver Jaws"
+
+// console.log(verPelicula);
+
+
+
+// for(let i = 1; i <= 100; i++){
+
+//     console.log("Hola David, eres un genio");
+// }
+
+let vida = -5;
+
+
+// while(vida >= 0){
+
+//     vida -= 5;
+//     console.log(vida);
+
+//     if(vida == 0){
+//         break;
+//     }
+
+// }
+
+do {
+
+    vida -= 5;
+    console.log(vida);
+
+    if(vida == 0){
+        break;
+    }
+
+} while (vida >= 0);
